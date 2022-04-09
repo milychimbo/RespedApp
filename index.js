cfb0f10d2b69b0a26e069387c6a70448af0ee23e
milychimbo <chimboemily@gmail.com>
2022-04-09 15:03:38 -0500

    fetch: agregar configuraciones iniciales

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express')
+const bodyParser =  require("body-parser");
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send(`<h1>Hello!</h1>`)
+});
+
+app.listen(port, () => {
+    console.log(`Application is listening at port ${port}`);
+});
