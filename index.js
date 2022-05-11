04533a8462b487668f4962c230cb134ffa81052b
milychimbo <chimboemily@gmail.com>
2022-05-11 00:37:14 -0500

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const morgan = require('morgan');
+//const morgan = require('morgan');
 
 
 
 const app = express();
 const port = 8000;
 
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
