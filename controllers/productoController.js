a64e5a57d80e36a054987184f687bb25e0d410ce
milychimbo <chimboemily@gmail.com>
2022-05-30 18:28:58 -0500

    fix dos decimales

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -19,8 +19,11 @@ const {
 
 async function obtenerProductos(req = request, res = response) {
     const productos = await getAllProductos();
-    if (productos.length > 0)
-        res.status(200).json(responseJson(200, "success", productos))
+    if (productos.length > 0){
+    productos.forEach(producto => {
+        producto.PRICE= addZeroes(producto.PRICE.toString());
+    });
+        res.status(200).json(responseJson(200, "success", productos))}
     else
         res.status(404).json(responseJson(404, "no existe"))
 }
@@ -34,6 +37,7 @@ async function obtenerMenu(req = request, res = response) {
         
         var obj1 = new Array;
         productos.forEach(producto => {
+            producto.PRICE= addZeroes(producto.PRICE.toString());
             if(producto.IDCATEGORIA == categoria.IDCATEGORIA){
                 
                 obj1.push(producto);
@@ -47,14 +51,16 @@ async function obtenerMenu(req = request, res = response) {
 
 async function obtenerProductoId(req = request, res = response) {
     const producto = await getOneProducto(req.params.id);
-    if (producto != null)
-        res.status(200).json(responseJson(200, "success", producto))
+    if (producto != null){
+        producto.PRICE= addZeroes(producto.PRICE.toString());
+        res.status(200).json(responseJson(200, "success", producto))}
     else
         res.status(404).json(responseJson(404, "no existe"))
 }
 
 async function crearProducto(req = request, res = response) {
     const producto = await createProducto(req.body);
+    
     if (Object.keys(producto)[0]=="dataValues"){
         res.status(200).json(responseJson(200, "success"))}
         else
@@ -77,6 +83,19 @@ async function borrarProducto(req = request, res = response) {
         res.status(200).json(responseJson(200, "no hubo cambios"))
 }
 
+function addZeroes(num) {
+    // Convert input string to a number and store as a variable.
+        var value = Number(num);      
+    // Split the input string into two arrays containing integers/decimals
+        var res = num.split(".");     
+    // If there is no decimal point or only one decimal place found.
+        if(res.length == 1 || res[1].length < 3) { 
+    // Set the number to two decimal places
+            value = value.toFixed(2);
+        }
+    // Return updated or original number.
+    return value;
+    }
 
 module.exports = {
     obtenerProductos,
