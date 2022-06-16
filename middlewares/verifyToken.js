6dd70703320698a661843d4ede3c0a8f84eebbc1
milychimbo <chimboemily@gmail.com>
2022-06-16 10:51:10 -0500

    volver a bearer

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -11,18 +11,21 @@ const {
 const secret = process.env.SECRET || 'CDjNU7uuZWazUSQsScR/P5RYwSeTsm2I0HLCUXKWnHY';
 
 function validateToken(req = request, res = response, next) {
-    let token = req.cookies.SESSION_ID;
-    console.error(req.cookies);
-    if (token != undefined) {
-        try {
-            const decodedToken = jwt.verify(token, secret);
-            req.currentToken = decodedToken;
-            next();
-        } catch {
-            res.status(401).json(responseJson(401, "no autorizado"))
+    const authorization = req.headers.authorization;
+    let token = null;
+    if (authorization != undefined) {
+        if (authorization.startsWith('Bearer')) {
+            token = authorization.substring(7);
+            try {
+                const decodedToken = jwt.verify(token, secret);
+                req.currentToken=decodedToken;
+                next();
+            } catch {
+                res.status(401).json(responseJson(401, "no autorizado"))
+            }
         }
     } else {
         res.status(401).json(responseJson(401, "no autorizado"))
     }
 }
-module.exports = { validateToken }
+module.exports = {validateToken}
