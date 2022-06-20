aa304304cf94992a7403daa73b166002f21f0da2
milychimbo <chimboemily@gmail.com>
2022-06-20 05:09:37 -0500

    corregir validador pedido

--- LogicLayer/validator/productoValidator.js
+++ LogicLayer/validator/productoValidator.js
@@ -6,8 +6,7 @@ const validationInsert = [
     check('DETAIL').optional().isString().trim().withMessage('Debe ser de tipo texto'),
     check('DETAIL').optional().isLength({max: 200 }).withMessage('Longitud máxima de 200'),
     check('PRICE').not().isEmpty().isFloat().withMessage('No es un valor valido'),
-    check('AVAILABILITY').not().isEmpty().isInt({ gt: 0, lt: 3 }).withMessage('Debe ser un valor del 1-2'),
-    check('AVAILABILITY').isLength({ min: 1, max: 1 }).withMessage('Longitud de 1'),
+    check('AVAILABILITY').not().isEmpty().isBoolean().withMessage('Debe ser un valor booleano'),
     check('IMAGE').optional().isURL().trim().withMessage('Debe ser de tipo URL'),
     check('IDCATEGORIA').isInt().trim().withMessage('No es un valor valido'),
     (req, res, next) => {
@@ -22,7 +21,7 @@ const validationUpdate = [
     check('DETAIL').optional().isString().trim().withMessage('Debe ser de tipo texto'),
     check('DETAIL').optional().isLength({max: 200 }).withMessage('Longitud máxima de 200'),
     check('PRICE').optional().isFloat().withMessage('No es un valor valido'),
-    check('AVAILABILITY').optional().isInt({ gt: 0, lt: 3 }).withMessage('Debe ser un valor del 1-2'),
+    check('AVAILABILITY').optional().isBoolean().withMessage('Debe ser un valor del 1-2'),
     check('AVAILABILITY').optional().isLength({ min: 1, max: 1 }).withMessage('Longitud de 1'),
     check('IMAGE').optional().isURL().trim().withMessage('Debe ser de tipo URL'),
     
