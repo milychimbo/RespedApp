522e8f9278c8fd3e3160fa42dcf6672473fc8130
milychimbo <chimboemily@gmail.com>
2022-06-20 05:06:11 -0500

    cambiar tipo boolean producto

--- DataLayer/producto.js
+++ DataLayer/producto.js
@@ -37,7 +37,7 @@ const Producto = connection.define('PRODUCTO', {
         type: DataTypes.TEXT
     },
     AVAILABILITY: {
-        type: DataTypes.TINYINT,
+        type: DataTypes.BOOLEAN,
         allowNull: false
     }
 }, {
